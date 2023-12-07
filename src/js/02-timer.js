db7501e406736a31302d762a502caa52d732e5f7
CristinaBente <128289401+CristinaBente@users.noreply.github.com>
2023-12-07 22:45:22 +0200

    se

---
+++ src/js/02-timer.js
@@ -0,0 +1,14 @@
+// Descris în documentație
+import flatpickr from 'flatpickr';
+// Import suplimentar de stil
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
